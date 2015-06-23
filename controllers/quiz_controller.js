58fc678499885d08a3b193846773edcbec2a1345
mrgiron <mrgiron@gmail.com>
2015-06-22 23:36:54 -0600

    Despliegue en Heroku

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -11,3 +11,8 @@ exports.answer = function(req, res){
 		res.render('quizes/answer', {respuesta: 'Incorrecto'});
 	}
 };
+
+//get /author
+exports.author = function(req, res){
+	res.render('quizes/author');
+};
